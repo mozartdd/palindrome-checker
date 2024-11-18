5e4b542fb374335a82a7d1498cacc9a5208d2ea1
Dainis Dilevka <mozart_dd@outlook.com>
2024-11-18 11:46:15 +0000

    completed palindrome logic

--- script.js
+++ script.js
@@ -2,24 +2,19 @@ const button = document.querySelector("#check-btn");
 const input = document.querySelector("#text-input");
 const p = document.querySelector("#result");
 
-
 /* event listener attached to button which is checking is 
 string in input a palindrome or not */
 button.addEventListener("click", () => {
     if (input.value === "") {
         alert("Please input a value");
-    } else if (input.value === input.value.split('').reverse().join('')) {
+    } else {
+        let cleanedInput = input.value.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    if (cleanedInput === cleanedInput.split('').reverse().join('')) {
         p.textContent = `${input.value} is a palindrome`;
-        input.value = "";
     } else {
         p.textContent = `${input.value} is not a palindrome`;
-        input.value = "";
     }
+    input.value = ""
+    }   
 });
-
-// TO DO: make function to exclude special characters, space and upper/lower case letters 
-const regex = (rgx) => {
-    rgx = /_/;
-}
-
-
